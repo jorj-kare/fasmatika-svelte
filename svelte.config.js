eb64517aee66712ff98eb2de9b75d05b2677ef81
jorj-kare <jorj.kare@gmail.com>
2023-02-01 19:08:21 +0100

    config scss prepend data

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,9 @@ import adapter from '@sveltejs/adapter-netlify';
 import preprocessor from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocessor(),
+	preprocess: preprocessor({
+		scss: { prependData: '@use "static/sass/abstracts" as * ;' }
+	}),
 	kit: {
 		adapter: adapter()
 	}
