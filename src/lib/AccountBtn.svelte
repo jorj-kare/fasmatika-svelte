52dcd55e43a1e09ef6640d1a1595a694a03c3061
jorj-kare <jorj.kare@gmail.com>
2023-02-01 19:04:01 +0100

    refactor:(AccountBtn)

--- src/lib/AccountBtn.svelte
+++ src/lib/AccountBtn.svelte
@@ -1,24 +1,28 @@
 <script>
+	export let href;
+	export let text;
 </script>
 
-<button>
-	<slot />
-</button>
+<a {href}>
+	{text}
+</a>
 
-<style>
-	button {
+<style lang="scss">
+	a {
 		border: none;
 		padding: 0.5rem;
+		background-color: $color-white;
+		color: $color-black;
 		font-size: 1.2rem;
 		letter-spacing: 0.1rem;
-		transition: color 1.5s ease-in, border 2s, background-color 0.8s;
-	}
-	button:hover,
-	button:active {
-		background-color: transparent;
-		color: #eee;
-		border-radius: 5px;
-		border-left: 0.5px solid #eee;
-		border-right: 0.5px solid #eee;
+		transition: color 1.5s ease-in, border 2s, border-radius 1s, background-color 0.8s;
+
+		&:hover {
+			background-color: transparent;
+			color: #eee;
+			border-radius: 5px;
+			border-left: 0.5px solid #eee;
+			border-right: 0.5px solid #eee;
+		}
 	}
 </style>
